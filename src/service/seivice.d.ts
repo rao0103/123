255df4261d4cdf1721ecd810fc414e9d594ef072
yaosong <3483988573@qq.com>
2023-07-28 20:22:55 +0800

    feat: 初始化项目
    初始化项目

--- src/service/seivice.d.ts
+++ src/service/seivice.d.ts
@@ -1,10 +1,1 @@
-interface IBaseResponse<T = null> {
-  code: 0 | 404;
-  data: T;
-  msg: string;
-}
-type ILoginResponse = IBaseResponse<{
-  accessToken: string;
-  csrf: string;
-  refreshToken: string;
-}>;
+
